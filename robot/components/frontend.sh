703f115cee0ea0333a4780d460077ae903d12c6f
tejash raju <tejashraju@tejashs-Air.lan>
2023-03-17 17:26:21 -0400

    Auto push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -e
 USER_ID=$(id -u)
 
 if ["USER_ID" -ne 0]; then
     echo -e "\e[32m you need to execute this script as root user or use a sudo as prefix \e[0m"
     exit 1
-if
+fi
 
 yum install nginx -y
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
@@ -18,3 +17,6 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+systemctl start nginx
