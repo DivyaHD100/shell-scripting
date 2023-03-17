0e88e6cbc5699137780cb8e968016ae111224b9c
tejash raju <tejashraju@tejashs-Air.lan>
2023-03-17 17:34:59 -0400

    Auto push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -3,7 +3,7 @@
 set -e
 ID=$(id -u)
 
-if ["$ID" -ne 0]; then
+if [ "$ID" -ne 0 ]; then
     echo -e "\e[30m you need to execute this script as root user or use a sudo as prefix \e[0m"
     exit 1
 fi
