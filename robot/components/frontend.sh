cc6d835d2dd28c74e4c556524681d8d0228484b2
tejash raju <tejashraju@tejashs-Air.lan>
2023-03-17 17:55:53 -0400

    Auto push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -19,15 +19,24 @@ echo -n "Installing the nginx:"
 yum install nginx -y &>> /tmp/frontend.log
 Stat $?
 
+echo -n "Downloading the frontend component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
+Stat $?
 
+echo -n "Performing cleanup of old frontend content:"
+cd /usr/share/nginx/html
 rm -rf * &>> /tmp/frontend.log
+Stat $?
+
+echo -n "Copying the downloaded frontend content:"
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+Stat $?
 
+echo -n "Starting the Nginx:"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx &>> /tmp/frontend.log
+Stat $?
