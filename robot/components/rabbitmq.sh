21a98516ae828a23c96315cf6ceb2ff24e234c13
tejash raju <tejashraju@tejashs-Air.lan>
2023-03-23 21:56:43 -0400

    Autopush

---
+++ robot/components/rabbitmq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#set -e
+COMPONENT=rabbitmq
+source components/common.sh
+
+echo -n "INstalling and Configuring dependency:"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$LOGFILE
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$LOGFILE
+Stat $?
+
+
+echo -n "Installing $COMPONENT:"
+yum install rabbitmq-server -y &>>$LOGFILE
+Stat $?
+
+echo -n "Starting $COMPONENT :"
+systemctl enable $COMPONENT-server &>>$LOGFILE
+systemctl start $COMPONENT-server &>>$LOGFILE
+Stat $?
+
+echo -n "Creating $COMPONENT Application user:"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOGFILE
+Stat $?
+
+
