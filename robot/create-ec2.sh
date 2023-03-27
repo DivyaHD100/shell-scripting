410cb325e23e5050be4429952748487dd489597c
tejash raju <tejashraju@tejashs-Air.lan>
2023-03-27 16:50:51 -0400

    Autopush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -16,4 +16,5 @@ aws ec2 run-instances --image-id $AMI_ID \
                     --instance-type t2.micro \
                     --security-group-ids ${SGID} \
                     --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                    --tag-specifications"ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq #'.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+                    --tag-specifications 
+                    "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq #'.Instances[].PrivateIpAddress' | sed -e 's/"//g')
