3ad3ca2f83c8b6b397e6878abe59aa33f5cb78a5
tejash raju <tejashraju@tejashs-Air.lan>
2023-03-27 16:09:52 -0400

    Autopush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].Imageid')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId')
 echo " AMI id is $AMI_ID "
