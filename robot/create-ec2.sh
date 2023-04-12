84b23704f768c62085ce2a8d5eb891e9a8a61267
tejash raju <tejashraju@tejashs-MacBook-Air.local>
2023-04-11 23:18:05 -0400

    Autopush

--- robot/create-ec2.sh
+++ robot/create-ec2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
     echo -e "\e[31m Component name is required \e[0m\t\t"
-    echo -e "\t\t\t \e[32m Sample usage is: $bash create-ec2.sh user \e[0m\t\t"
+    echo -e "\t\t\t \e[32m Sample usage is: $bash create-ec2.sh user dev \e[0m\t\t"
     exit 1
 fi
 HOSTEDZONEID="Z01460382LDVHUYMKWW6K"
 COMPONENT=$1
+ENV=$2
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b53-allow-all | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
 echo " AMI id is $AMI_ID "
@@ -21,9 +23,9 @@ IPADDRESS=$(aws ec2 run-instances --image-id $AMI_ID \
                     --instance-type t2.micro \
                     --security-group-ids ${SGID} \
                     --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+                    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT-$ENV}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-sed -e "s/COMPONENT/${COMPONENT}/" -e  "s/IPADDRESS/${IPADDRESS}/" record.json > /tmp/r53.json
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/" -e  "s/IPADDRESS/${IPADDRESS}/" record.json > /tmp/r53.json #search for Component in record.json and replace with COMPONENt-ENV
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json | jq
     echo "*** $COMPONENT server completed ***"
 }
