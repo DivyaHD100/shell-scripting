9c149e75d354876912f8e292e995a8d41ef494ba
tejash raju <tejashraju@tejashs-Air.lan>
2023-08-03 16:39:29 -0400

    Autopush

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -20,13 +20,13 @@ Stat(){
     fi 
 }
 
-echo -n "Configuring $COMPONENT repo:"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo  &>>$LOGFILE
-Stat $?
+echo -n "Configuring $COMPONENT repo :"
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> $LOGFILE
+stat $? 
 
-echo -n "Installing $COMPONENT server:"
+echo -n "Installing $COMPONENT server :"
 yum install redis-6.2.11 -y  &>> $LOGFILE
-Stat $?
+stat $?
 
 echo -n "Updating the $COMPONENT visibility:"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
