f2d723b54791598c9c6b2194086666bd477c29d7
tejash raju <tejashraju@tejashs-Air.lan>
2023-08-03 16:49:53 -0400

    Autopush

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -25,7 +25,7 @@ curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.r
 stat $? 
 
 echo -n "Installing $COMPONENT server :"
-yum install redis-6.2.11 -y  &>> $LOGFILE
+yum install redis-6.2.6 -y  &>> $LOGFILE
 stat $?
 
 echo -n "Updating the $COMPONENT visibility:"
